c8ae5f45f07b63eeaefb35fd0360a4607f488c36
Pasztor, A. (Arthur) <>
2023-03-28 16:58:43 +0200

    Add Ktor HttpClient and call rocket launch API

--- shared/src/commonMain/kotlin/com/example/myfirstkmm/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/myfirstkmm/Greeting.kt
@@ -1,10 +1,40 @@
 package com.example.myfirstkmm
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
+    fun oldGreet(): String {
         return "Guess what it is, ${platform.name.reversed()}!" +
                 "\nThere are only ${daysUntilNewYear()} left until New Year! \uD83C\uDF86"
+    }
+
+    @Throws(Exception::class)
+    suspend fun greet(): String {
+        val rockets: List<RocketLaunch> = httpClient.get(API_URL).body()
+        val lastSuccessLaunch = rockets.last { it.launchSuccess == true }
+        return "Guess what it is, ${platform.name.reversed()}!" +
+                "\nThere are only ${daysUntilNewYear()} left until New Year! \uD83C\uDF86" +
+                "\nThe last successful launch was ${lastSuccessLaunch.launchDateUTC} \uD83D\uDE80"
+    }
+
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    companion object {
+        private const val API_URL = "https://api.spacexdata.com/v4/launches"
     }
 }
