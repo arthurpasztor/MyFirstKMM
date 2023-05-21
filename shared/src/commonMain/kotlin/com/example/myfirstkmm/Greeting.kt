7abf751cf50f798004ea09b384c10e561f1f4f20
Pasztor, A. (Arthur) <>
2023-05-21 13:07:35 +0300

    Add ktor logging

--- shared/src/commonMain/kotlin/com/example/myfirstkmm/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/myfirstkmm/Greeting.kt
@@ -1,11 +1,8 @@
 package com.example.myfirstkmm
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
+import com.example.myfirstkmm.HttpClient.httpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
 class Greeting {
     private val platform: Platform = getPlatform()
@@ -22,16 +19,6 @@ class Greeting {
         return "Guess what it is, ${platform.name.reversed()}!" +
                 "\nThere are only ${daysUntilNewYear()} left until New Year! \uD83C\uDF86" +
                 "\nThe last successful launch was ${lastSuccessLaunch.launchDateUTC} \uD83D\uDE80"
-    }
-
-    private val httpClient = HttpClient {
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient = true
-                ignoreUnknownKeys = true
-            })
-        }
     }
 
     companion object {
---
+++ shared/src/commonMain/kotlin/com/example/myfirstkmm/HttpClient.kt
@@ -0,0 +1,26 @@
+package com.example.myfirstkmm
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClient {
+    val httpClient = HttpClient {
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.ALL
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+}
